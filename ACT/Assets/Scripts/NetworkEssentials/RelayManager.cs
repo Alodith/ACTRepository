94962d96f0ff5c9c29a7e38f667c958f11971d5d
Cameron Yates <alodith15@gmail.com>
2025-08-17 16:57:56 +0100

    Buttons disappear on join44

--- ACT/Assets/Scripts/NetworkEssentials/RelayManager.cs
+++ ACT/Assets/Scripts/NetworkEssentials/RelayManager.cs
@@ -14,6 +14,7 @@ public class RelayManager : MonoBehaviour
 
     [SerializeField] private TextMeshProUGUI joinCodeText;
     [SerializeField] private TMP_InputField joinCodeInputField;
+    [SerializeField] private GameObject thingsToDisable;
 
     private async void Start()
     {
@@ -26,11 +27,13 @@ public class RelayManager : MonoBehaviour
     {
         string joinCode = await StartHostWithRelay();
         joinCodeText.text = joinCode;
+        thingsToDisable.SetActive(false);
     }
 
     public async void JoinRelay()
     {
         await StartClientWithRelay(joinCodeInputField.text);
+        thingsToDisable.SetActive(false);
     }
 
 
@@ -46,6 +49,7 @@ public class RelayManager : MonoBehaviour
         string joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
 
         return NetworkManager.Singleton.StartHost() ? joinCode : null;
+
     }
     
     private async Task<bool> StartClientWithRelay(string joinCode) 
